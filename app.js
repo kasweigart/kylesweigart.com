63bcc8b9b610dfbc6d970bff3f100ecda45f4322
Kyle Sweigart <kasweigart@gmail.com>
2020-05-21 16:19:42 -0400

    app.js correction

--- app.js
+++ app.js
@@ -21,7 +21,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.listen(port);
-console.log('Listening on port ' + port + '...');
 
 module.exports = app;
