21072f1af12f02a345e35a8b57adddc155149fda
Kyle Sweigart <kasweigart@gmail.com>
2020-05-22 18:15:50 -0400

    finished css div layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,36 @@
 import React from 'react';
-import './App.css';
-
-
-  
+import './css/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="main">
+      <header className="header">
+        <div className="headerContent">
+          <div className="logo">
+            Kyle Sweigart
+          </div>
+          <div className="nav">
+          <div className="about">
+            ABOUT
+          </div>
+          <div className="portfolio">
+            PORTFOLIO
+          </div>
+          <div className="contact">
+            CONTACT
+          </div>
+          </div>
+        </div>
       </header>
+      <div className="mainContent">
+
+      </div>
+      <div className="secContent">
+        
+      </div>
+      <div className="footer">
+      © 2020 Kyle Sweigart
+      </div>
     </div>
   );
 }
