05935a1860c74f94c0a9222d022768766b2ef1e9
Kyle Sweigart <kasweigart@gmail.com>
2020-06-02 22:35:59 -0400

    deleted build dir

--- server.js
+++ server.js
@@ -4,14 +4,18 @@ const path = require('path');
 const port = 3001;
 const apiRouter = require('./routes');
 
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+app.use(apiRouter);
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
-app.use(express.json());
-app.use(apiRouter);
+
+
 
 app.listen(port, "127.0.0.1");
 console.log(`Listening on port ${port}...`);
